85febdfcb7bbeebb0971b2a1cae32337bb353ffc
ree17017 <ree17017@byui.edu>
2021-08-15 00:52:04 -0600

    feat: Add Display endpoint

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+// Fake data
+const expenseListFake = [
+  { id: 123, expense: "item 1", value: 100 },
+  { id: 124, expense: "item 2", value: 100 },
+];
+
+const displayFake = { budget: 100, expenses: 100, balance: 100 };
+
+// Sudo code
+// post Budget value
+// post Expense name and amount
+// get display values
+app.get("/display", function (req, res) {
+  res.send(displayFake);
+});
+
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
